db63085c46adf2361eacd7b41f3eec6af2dc18d0
Max Moreno <rodney.moreno@outlook.com>
2026-02-03 22:04:14 -0800

    get_embeddings function

--- lib/job_hunting_ex/data.ex
+++ lib/job_hunting_ex/data.ex
@@ -1,6 +1,6 @@
 defmodule JobHuntingEx.Data do
   def polite_sleep do
-    :timer.sleep(Enum.random(1..5))
+    :timer.sleep(Enum.random(1000..3000))
   end
 
   def get_urls(params) do
@@ -20,5 +20,28 @@ defmodule JobHuntingEx.Data do
   end
 
   def get_embeddings(documents) do
+    body = %{"model" => "qwen/qwen3-embedding-8b", "input" => elem(documents, 1)}
+
+    response =
+      Req.post!(
+        url: "https://openrouter.ai/api/v1/embeddings",
+        headers: [
+          authorization:
+            "Bearer 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",
+          content_type: "application/json"
+        ],
+        json: body
+      )
+
+    Enum.map(response.body["data"], & &1["embedding"])
+    |> Enum.zip(elem(documents, 0))
+  end
+
+  def process(params) do
+    get_urls(params)
+    |> Task.async_stream(fn url -> get_html(url) end, max_concurrency: 2, ordered: false)
+    |> Stream.chunk_every(25)
+    |> Task.async_stream()
+    |> IO.inspect()
   end
 end
