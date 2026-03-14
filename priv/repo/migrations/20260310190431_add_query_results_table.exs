a6fb68dfb278629082d6a2001ec8a88b7e56c884
Max Moreno <rodney.moreno@outlook.com>
2026-03-14 12:24:12 -0700

    Revert to autogenerated primary_key in query_result table

--- priv/repo/migrations/20260310190431_add_query_results_table.exs
+++ priv/repo/migrations/20260310190431_add_query_results_table.exs
@@ -2,8 +2,8 @@ defmodule JobHuntingEx.Repo.Migrations.AddQueryResultsTable do
   use Ecto.Migration
 
   def change do
-    create table(:query_results, primary_key: false) do
-      add :job_id, :uuid, primary_key: true, null: false
+    create table(:query_results) do
+      add :job_id, :uuid, null: false
       add :listing_id, :integer, null: false
       add :sequence, :integer, null: false
     end
