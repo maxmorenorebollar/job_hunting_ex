a6e4eda3f58a37ee72d1356f3233ff32480d63cf
Max Moreno <rodney.moreno@outlook.com>
2026-03-14 13:55:23 -0700

    Refactor pdf.ex and add typespec

--- lib/job_hunting_ex/queries/pdf.ex
+++ lib/job_hunting_ex/queries/pdf.ex
@@ -1,11 +1,16 @@
 defmodule JobHuntingEx.Queries.Pdf do
+  @moduledoc """
+  Provides function to extract text from pdf
+  """
+
+  @spec extract_text(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def extract_text(pdf_path) do
-    IO.inspect(pdf_path)
     args = [pdf_path, "-"]
 
-    with {text, 0} <- System.cmd("pdftotext", args) do
-      {:ok, String.replace(text, "\n", " ")}
-    else
+    case System.cmd("pdftotext", args) do
+      {:ok, text} ->
+        String.replace(text, "\n", " ")
+
       {error, exit_status} ->
         {:error, "pdftotext failed with exit_status #{exit_status} and reason: #{error}"}
     end
