46811a141a657ca93c573b96c6d93b33ef03b66e
Max Moreno <rodney.moreno@outlook.com>
2026-03-14 14:56:32 -0700

    run format

--- lib/job_hunting_ex_web/controllers/query_controller.ex
+++ lib/job_hunting_ex_web/controllers/query_controller.ex
@@ -2,7 +2,6 @@ defmodule JobHuntingExWeb.QueryController do
   use JobHuntingExWeb, :controller
 
   def show(conn, %{"id" => id}) do
-
     case JobHuntingEx.Queries.get_listings(id) do
       [] -> render(conn, :show, listings: [])
       listings -> render(conn, :show, listings: listings)
