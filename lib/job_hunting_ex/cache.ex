115ee9c7a8bc36b3e16d0f80f4dcff6d40d1375c
Max Moreno <rodney.moreno@outlook.com>
2026-03-15 17:30:17 -0700

    fix write through cache add typespecs

--- lib/job_hunting_ex/cache.ex
+++ lib/job_hunting_ex/cache.ex
@@ -8,17 +8,23 @@ defmodule JobHuntingEx.Cache do
     259_200_000
   end
 
-  def write_through(listings) do
+  @spec write_through([]) :: {:ok, []}
+  def write_through(listings) when listings == [] do
+    {:ok, []}
+  end
+
+  @spec write_through(list(map())) ::
+          {:ok, list(map())} | {:error, :transaction_failed} | {:error, :cache_failed}
+  def(write_through(listings)) do
     listings_as_kv =
       Enum.map(listings, fn listing -> {listing.url, listing} end)
 
-    if listings_as_kv != [] do
-      case Cachex.put_many(:cache, listings_as_kv, expire: time_to_live()) do
-        {:ok, true} -> Listings.create_all(Enum.map(listings, &Map.from_struct/1))
-        {:ok, false} -> {:error, :cache_failed}
-      end
+    with {:ok, listings} <- Listings.create_all(Enum.map(listings, &Map.from_struct/1)),
+         {:ok, true} <- Cachex.put_many(:cache, listings_as_kv, expire: time_to_live()) do
+      {:ok, listings}
     else
-      {:ok, []}
+      {:error, :transaction_failed} -> {:error, :transaction_failed}
+      {:ok, false} -> {:error, :cache_failed}
     end
   end
 end
