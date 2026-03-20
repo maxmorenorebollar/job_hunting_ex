0e6140837c7aa6ef25ecdeba4f4eea4bfaa15179
Max Moreno <rodney.moreno@outlook.com>
2026-03-19 19:57:04 -0700

    force ssl, remove server: true in production config

--- config/prod.exs
+++ config/prod.exs
@@ -11,9 +11,7 @@ config :job_hunting_ex, JobHuntingExWeb.Endpoint,
 # Force using SSL in production. This also sets the "strict-security-transport" header,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
-config :job_hunting_ex, JobHuntingExWeb.Endpoint,
-  server: true
-  # force_ssl: [rewrite_on: [:x_forwarded_proto]]
+config :job_hunting_ex, JobHuntingExWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Configure Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
