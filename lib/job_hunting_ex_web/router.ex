fb64a6989010ff54e895f14487275899eb812832
Max Moreno <rodney.moreno@outlook.com>
2026-03-22 12:34:37 -0700

    run mix.auth.gen

--- lib/job_hunting_ex_web/router.ex
+++ lib/job_hunting_ex_web/router.ex
@@ -1,6 +1,8 @@
 defmodule JobHuntingExWeb.Router do
   use JobHuntingExWeb, :router
 
+  import JobHuntingExWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule JobHuntingExWeb.Router do
     plug :put_root_layout, html: {JobHuntingExWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -23,7 +26,7 @@ defmodule JobHuntingExWeb.Router do
 
     live "/", QueryLive.New
 
-    get "/*_", PageController, :to_home
+    # get "/*_", PageController, :to_home
   end
 
   # Other scopes may use custom stacks.
@@ -46,5 +49,33 @@ defmodule JobHuntingExWeb.Router do
       live_dashboard "/dashboard", metrics: JobHuntingExWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", JobHuntingExWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{JobHuntingExWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", JobHuntingExWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{JobHuntingExWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
